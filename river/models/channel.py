4a0e9549b837f70b7f4fd92f8e92ec247bc64d6a
Andrey Sanchez <s.4ndrey@gmail.com>
2025-01-06 11:05:15 -0600

    Adds the number of variables to the model.

--- river/models/channel.py
+++ river/models/channel.py
@@ -3,6 +3,7 @@ from django.db import models
 class Channel(models.Model):
     name = models.CharField(max_length=100)
     template = models.CharField(max_length=500, blank=False)
+    number_of_variables = models.IntegerField(default=0)
 
     class Meta:
         app_label = 'river'
