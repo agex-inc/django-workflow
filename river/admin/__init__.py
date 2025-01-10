5764a651e84f9ad4583afd07ded7793ee158da99
Alejandro Arce <aarce@akurey.com>
2025-01-11 02:54:59 +0800

    make actor model readonly

--- river/admin/__init__.py
+++ river/admin/__init__.py
@@ -1,3 +1,4 @@
+from river.admin.actor_admin import *
 from river.admin.function_admin import *
 from river.admin.hook_admins import *
 from river.admin.transitionapprovalmeta import *
@@ -6,11 +7,9 @@ from river.admin.workflow import *
 from river.models import State
 from river.models import Channel
 from river.models import Template
-from river.models import Actor
 from river.models import NotificationTemplate
 
 admin.site.register(State)
 admin.site.register(Channel)
 admin.site.register(Template)
-admin.site.register(Actor)
 admin.site.register(NotificationTemplate)
