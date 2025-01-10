a1e37eda86984dbb8689be743096e79b63b58ea1
Alejandro Arce <aarce@akurey.com>
2025-01-10 22:55:19 +0800

    update template model to add help text

--- river/models/template.py
+++ river/models/template.py
@@ -2,7 +2,9 @@ from django.db import models
 
 class Template(models.Model):
     name = models.CharField(max_length=100, null=True, blank=True)
-    text = models.TextField()
+    text = models.TextField(
+        help_text="You can use variables like {{borrower_name}} and {{seller_name}} which will be automatically replaced with the corresponding value."
+    )
     channel = models.ForeignKey('Channel', on_delete=models.CASCADE)
     actor = models.ForeignKey('Actor', null=True, blank=True, on_delete=models.CASCADE)
 
