a5237ae654a01093326aa623db8d290e9c2f2ca3
Andrey Sanchez <s.4ndrey@gmail.com>
2025-01-13 14:42:28 -0600

    Revert "Changing the name from river to config"
    This reverts commit 0969eacf629921f5cebf02ecf90fd4a4d3c1397c.

--- river/apps.py
+++ river/apps.py
@@ -11,7 +11,6 @@ LOGGER = logging.getLogger(__name__)
 class RiverApp(AppConfig):
     name = 'Workflow Configuration'
     label = 'Workflow Configuration'
-    verbose_name = 'Workflow Configuration'
 
     def ready(self):
         for field_name in self._get_all_workflow_fields():
