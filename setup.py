784519743971e06b532788ab4a479cbce998a08f
Andrey Sanchez <s.4ndrey@gmail.com>
2025-01-31 16:01:00 -0600

    bumping project version

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ except IOError as err:
 
 setup(
     name='django-workflow',
-    version='3.3.13',
+    version='3.3.14',
     author='Ahmet DAL',
     author_email='ceahmetdal@gmail.com',
     packages=find_packages(),
